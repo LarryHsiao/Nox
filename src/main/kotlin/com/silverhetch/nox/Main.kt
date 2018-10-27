5326d0572a5b7337eb81bdfa3b60bb8dd7e1a31c
LarryHsiao <bleakeraser1997@gmail.com>
2018-10-28 03:59:53 +0800

    database path in argument

--- src/main/kotlin/com/silverhetch/nox/Main.kt
+++ src/main/kotlin/com/silverhetch/nox/Main.kt
@@ -2,6 +2,7 @@ package com.silverhetch.nox
 
 import com.silverhetch.clotho.database.SingleConn
 import com.silverhetch.clotho.database.sqlite.InMemoryConn
+import com.silverhetch.clotho.database.sqlite.SQLiteConn
 import com.silverhetch.clotho.log.BeautyLog
 import com.silverhetch.nox.model.NoxDbConn
 import com.silverhetch.nox.model.log.DbLogQueryAll
@@ -23,14 +24,16 @@ import org.takes.http.FtBasic
 fun main(arg: Array<String>) {
     val cmd = DefaultParser().parse(Options().apply {
         addOption(Option("p", "port", true, "Running Port"))
+        addOption(Option("d", "database", true, "Database path"))
     }, arg)
 
     val port = cmd.getOptionValue("port", "8080").toInt()
+    val databasePath = cmd.getOptionValue("database", ":memory:")
 
     val log = BeautyLog().fetch()
     val dbConn = SingleConn(
         NoxDbConn(
-            InMemoryConn()
+            SQLiteConn(databasePath)
         )
     )
     val tkShutdown = TkShutdown()
