9bc5aa7afc833a57608a2f489676a26eea1196d0
LarryHsiao <bleakeraser1997@gmail.com>
2018-10-27 17:10:21 +0800

    Log type test

---
+++ src/test/kotlin/com/silverhetch/nox/LogTypeTest.kt
@@ -0,0 +1,35 @@
+package com.silverhetch.nox
+
+import org.junit.Assert.assertEquals
+import org.junit.Test
+
+class LogTypeTest {
+    @Test
+    fun simple() {
+        assertEquals(
+            LogType.INFO,
+            LogType.fromString("1234")
+        )
+        assertEquals(
+            LogType.INFO,
+            LogType.fromString("INFO")
+        )
+        assertEquals(
+            LogType.WARNING,
+            LogType.fromString("WARNING")
+        )
+        assertEquals(
+            LogType.ERROR,
+            LogType.fromString("ERROR")
+        )
+
+        assertEquals(
+            LogType.VERBOSE,
+            LogType.fromString("VERBOSE")
+        )
+        assertEquals(
+            LogType.DEBUG,
+            LogType.fromString("DEBUG")
+        )
+    }
+}
